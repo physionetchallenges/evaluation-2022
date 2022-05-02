405a98e1ccdd873811bc6ad978f151f0ac49b116
Matthew <mareyna@emory.edu>
2022-05-02 15:21:31 -0400

    Update code for official phase

--- evaluate_model.m
+++ evaluate_model.m
@@ -1,14 +1,13 @@
 % This file contains functions for evaluating models for the 2022 Challenge. You can run it as follows:
 %
-%   evaluate_model(labels, outputs, scores.csv, class_scores.csv)
+%   evaluate_model(labels, outputs, scores.csv)
 %
-# where 'labels' is a folder containing files with the labels, 'outputs' is a folder containing files with the outputs from your
-# model, 'scores.csv' (optional) is a collection of scores for the algorithm outputs, # and 'class_scores.csv' (optional) is a
-# collection of per-class scores for the algorithm outputs.
+% where 'labels' is a folder containing files with the labels, 'outputs' is a folder containing files with the outputs from your
+% model, and 'scores.csv' (optional) is a collection of scores for the model outputs.
 %
-# Each label or output file must have the format described on the Challenge webpage. The scores for the algorithm outputs include
-# the area under the receiver-operating characteristic curve (AUROC), the area under the recall-precision curve (AUPRC), accuracy,
-# macro F-measure, and the Challenge score.
+% Each label or output file must have the format described on the Challenge webpage. The scores for the algorithm outputs include
+% the area under the receiver-operating characteristic curve (AUROC), the area under the recall-precision curve (AUPRC), macro
+% accuracy, a weighted accuracy score, and the Challenge score.
 
 function evaluate_model(labels, outputs, output_file, class_output_file)
     % Check for Python and NumPy.
@@ -30,8 +29,6 @@ function evaluate_model(labels, outputs, output_file, class_output_file)
             command = ['python evaluate_model.py' ' ' labels ' ' outputs];
         case 3
             command = ['python evaluate_model.py' ' ' labels ' ' outputs ' ' output_file];
-        case 4
-            command = ['python evaluate_model.py' ' ' labels ' ' outputs ' ' output_file ' ' class_output_file];
         otherwise
             command = '';
     end
